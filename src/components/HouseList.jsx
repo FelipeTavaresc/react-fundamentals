e0b876f8cc5a593d6e821627fe01258ef3a50397
Felipe Tavares <felipe.tavares@outlook.com>
2026-05-29 19:17:36 -0300

    hooks

--- src/components/HouseList.jsx
+++ src/components/HouseList.jsx
@@ -1,4 +1,4 @@
-//import { useState } from "react";
+import { useState } from "react";
 import HouseRow from "./HouseRow";
 
 const houseArray = [
@@ -17,27 +17,44 @@ const houseArray = [
   ];
 
 const HouseList = () => {
+    const [houses, setHouses] = useState(houseArray);
+
+    const addHouse = () => {
+        setHouses([
+          ...houses,
+          {
+            id: 6,
+            address: "32 Valley Way, New York",
+            country: "USA",
+            price: 1000000,
+          },
+        ]);
+      };
+
     return (
         <>
-            <div className="row mb-2">
-                <h5 className="themeFontColor text-center">
-                    Houses currently on the market
-                </h5>
-            </div>
-            <table className="table table-hover">
-                <thead>
-                    <tr>
-                        <th>Address</th>
-                        <th>Country</th>
-                        <th>Asking Price</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {houseArray.map(h => <HouseRow key={h.id} house={h} {...h} />)}
-                </tbody>
-            </table>
+          <div className="row mb-2">
+            <h5 className="themeFontColor text-center">
+              Houses currently on the market
+            </h5>
+          </div>
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th>Address</th>
+                <th>Country</th>
+                <th>Asking Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              {houses.map(h => <HouseRow key={h.id} house={h} />)}
+            </tbody>
+          </table>
+          <button onClick={addHouse} className="btn btn-primary">
+            Add
+          </button>
         </>
-    )
-}
+     );
+};
 
 export default HouseList;
